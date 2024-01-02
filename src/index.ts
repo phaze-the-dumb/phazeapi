443b351590fdf7f5d9ca1f5cc3d13ab9ffe000c0
phaze-the-dumb <phaze@duck.com>
2024-01-02 21:05:25 +0000

    what the fuck is wrong with you

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,20 @@ fastify.get<{ Querystring: { key: String } }>('/api/update', ( req, reply ) => {
   if(req.query.key === process.env.MASTER_KEY)
     return reply.code(403).send({ ok: false });
 
+  console.log('Pulling github repo.');
+
   spawn('git', [ 'pull', 'origin' ]).on('close', () => {
-    console.log('Pulled github repo, building...');
+    console.log('Pulled github repo. Installing deps...');
 
-    spawn('npm', [ 'run', 'build' ]).on('close', () => {
-      console.log('Built code. Restarting...');
+    spawn('npm', [ 'install' ]).on('close', () => {
+      console.log('Installed deps. Building...');
 
-      reply.send({ ok: true });
-      spawn('service api restart');
+      spawn('npm', [ 'run', 'build' ]).on('close', () => {
+        console.log('Built code. Restarting...');
+
+        reply.send({ ok: true });
+        spawn('service api restart');
+      })
     })
   })
 })
