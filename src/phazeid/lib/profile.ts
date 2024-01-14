8df66a011c6e112cf564f0999bb05bd832304ee4
phaze-the-dumb <phaze@duck.com>
2024-01-14 17:09:40 +0000

    fix avatar upload endpoints

--- src/phazeid/lib/profile.ts
+++ src/phazeid/lib/profile.ts
@@ -257,6 +257,15 @@ export let main = async ( fastify: FastifyInstance, transport: Transporter ) => 
     reply.send({ ok: true });
   })
 
+  fastify.options('/id/v1/email/avatar', ( req, reply ) => {
+    reply.header('Content-Type', 'application/json');
+    reply.header('Access-Control-Allow-Origin', 'https://id.phazed.xyz');
+    reply.header("Access-Control-Allow-Methods", "PUT");
+    reply.header("Access-Control-Allow-Headers", "Content-Type");
+    
+    reply.send('200 OK');
+  })
+
   fastify.put<{ Querystring: { token: string }, Body: { img: string } }>(
     '/id/v1/profile/avatar', 
     {
