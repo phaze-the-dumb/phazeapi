f1ca715e9b804dc87c249f6483eb4367fa433258
phaze-the-dumb <phaze@duck.com>
2024-01-19 18:38:44 +0000

    fix password endpoint

--- src/phazeid/lib/auth.ts
+++ src/phazeid/lib/auth.ts
@@ -774,6 +774,15 @@ export let main = async ( fastify: FastifyInstance, transport: Transporter ) => 
     reply.send({ ok: true })
   })
 
+  fastify.options('/id/v1/auth/password', ( req, reply ) => {
+    reply.header('Content-Type', 'application/json');
+    reply.header('Access-Control-Allow-Origin', 'https://id.phazed.xyz');
+    reply.header("Access-Control-Allow-Methods", "PUT");
+    reply.header("Access-Control-Allow-Headers", "Content-Type");
+    
+    reply.send('200 OK');
+  })
+
   fastify.put<{ Querystring: { token: string }, Body: { password: string, mfaCode?: string, previousPass: string } }>(
     '/id/v1/auth/password', 
     {
