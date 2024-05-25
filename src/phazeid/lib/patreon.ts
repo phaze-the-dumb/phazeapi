761e2ccf252fdcfc577b08d982bb8b1d33014085
phaze-the-dumb <phaze@duck.com>
2024-05-25 21:33:35 +0100

    test shit

--- src/phazeid/lib/patreon.ts
+++ src/phazeid/lib/patreon.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from "fastify";
+import { findUserFromToken } from "../sessionUtils";
 
 export let main = async ( fastify: FastifyInstance ) => {
-  fastify.get<{ Querystring: { state: string } }>('/id/v1/patreon', ( req, reply ) => {
-    reply.redirect(`https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${process.env.PATREON_CLIENT_ID}&scope=identity&redirect_uri=https://api.phazed.xyz/id/v1/patreon/callback&state=${req.query.state}`);
+  fastify.get<{ Querystring: { token: string } }>('/id/v1/patreon', async ( req, reply ) => {
+    let { user } = await findUserFromToken(req, reply);
+    if(!user)return;
+
+    reply.redirect(`https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${process.env.PATREON_CLIENT_ID}&scope=identity&redirect_uri=https://api.phazed.xyz/id/v1/patreon/callback&state=${req.query.token}`);
   })
 
   fastify.get<{ Querystring: { code: string, state: string } }>('/id/v1/patreon/callback', async ( req, reply ) => {
@@ -16,9 +20,7 @@ export let main = async ( fastify: FastifyInstance ) => {
 
     let data = await dataReq.json();
 
-    console.log(data);
-
-    let userReq = await fetch('https://www.patreon.com/api/oauth2/v2/identity?include=memberships,campaigns', {
+    let userReq = await fetch('https://www.patreon.com/api/oauth2/v2/identity?fields%5Bmember%5D=patron_status,is_follower,full_name&include=memberships.currently_entitled_tiers', {
       headers: {
         'Authorization': 'Bearer ' + data.access_token
       }
